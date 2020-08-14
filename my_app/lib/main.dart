a0b0a9ab8b7345bd15b48859442117456d4ee34c
unknown <titiksha1sharma@gmail.com>
2020-08-14 17:35:45 +0530

    'my_app'

---
+++ my_app/lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random(); // Add this line.
+    return MaterialApp(
+      title: 'Random Name Generator',
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.blueGrey,
+      ),
+
+      home: RandomWords(),
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+//          //child: Text('Hello World'),   // Replace this text...
+//          //child: Text(wordPair.asPascalCase),  // With this text.
+//          child: RandomWords(),
+//        ),
+//      ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];            // NEW
+  final _saved = Set<WordPair>();     // NEW
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18); // NEW
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          // NEW lines from here...
+          builder: (BuildContext context) {
+            final tiles = _saved.map(
+                  (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          }, // ...to here.
+        ),
+    );
+
+
+  }
+
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);  // NEW
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(   // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),                // ... to here.
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+
+  }
+}
